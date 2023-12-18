ddbc5d8c1d1c5ecf425be08b1303ec9c36bf5bfe
Agenor2512 <noemie.vasseur2512@gmail.com>
2023-12-18 12:38:30 +0100

    Adoption button is functional

---
+++ src/components/AdoptionButton.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import style from "../styles/AdoptionButton.module.css"
+
+function AdoptionButton() {
+    const [buttonText, setButtonText] = useState("Adopt me !")
+
+    const handleClick = (text) => {
+        setButtonText(text)
+        alert("Thanks for your interest for this pet ! We're going to contact you for more informations.")
+    }
+
+  return (
+    <button className={style["adoption-button"]} onClick={() => handleClick("✔️")}>{buttonText}</button>
+  )
+}
+
+export default AdoptionButton
